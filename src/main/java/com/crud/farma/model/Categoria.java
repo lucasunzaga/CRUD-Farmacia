88eac2e9949963bf20351c819c83555ad351ce51
lucasunzaga <lucasunzaga87@gmail.com>
2024-12-16 14:39:25 -0300

    Contruindo a entidade Produto e o CRUD

--- src/main/java/com/crud/farma/model/Categoria.java
+++ src/main/java/com/crud/farma/model/Categoria.java
@@ -1,9 +1,13 @@
 package com.crud.farma.model;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -19,6 +23,9 @@ public class Categoria {
 	@NotBlank
 	@Size(min = 5, max = 100, message = "O atributo descricao deve conter no mínimo 05 e no máximo 100 caracteres")
 	private String nome;
+	
+	@OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL)
+	private List<Produto> produto;
 
 	public Long getId() {
 		return id;
@@ -36,6 +43,13 @@ public class Categoria {
 		this.nome = nome;
 	}
 
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
 
 	
 }
--- src/main/java/com/crud/farma/model/Produto.java
+++ src/main/java/com/crud/farma/model/Produto.java
@@ -1,5 +1,71 @@
 package com.crud.farma.model;
 
-public class Produto {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+    
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
+	private Long id;
+	
+	@NotBlank(message = "O atributo título é Obrigatório!") 
+	@Size(min = 5, max = 100, message = "O atributo título deve conter no mínimo 05 e no máximo 100 caracteres")
+	private String nome;
+	
+	@Positive (message = "O atributo preço deve ser maior que 0!")
+	private Float preco;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Float getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Float preco) {
+		this.preco = preco;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+
+	
+	
+	
 }
