767c27088f87e43a208dcd8f7c403d2060107ab3
lucasunzaga <lucasunzaga87@gmail.com>
2024-12-16 12:26:49 -0300

    Configurando o projeto (pom.xml e application properties)

---
+++ src/test/java/com/crud/farma/FarmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.farma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
